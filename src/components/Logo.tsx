3698ad6bc07c2f195f2d45aeab08030d9d055e7e
Marcelo Souza <souzadeveloper@gmail.com>
2025-08-02 21:30:29 -0300

    [dyad] Ajustando o logotipo no cabeçalho - wrote 1 file(s) + extra files edited outside of Dyad

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,9 +3,8 @@ import logoSrc from "../assets/images/logotipo.png";
 
 export const Logo = () => {
   return (
-    <Link to="/" className="flex items-center space-x-3">
-      <img src={logoSrc} alt="Pata Livre Logo" className="h-10 w-auto" />
-      <span className="text-2xl font-bold text-gray-800">Pata Livre</span>
+    <Link to="/">
+      <img src={logoSrc} alt="Pata Livre Logo" className="h-14 w-auto" />
     </Link>
   );
 };
