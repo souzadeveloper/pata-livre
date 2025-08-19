b230d36c86770e5340e09604ea92ebc8a427a5f1
Marcelo Souza <souzadeveloper@gmail.com>
2025-08-19 02:00:19 -0300

    [dyad] Reducing space in the footer - wrote 1 file(s) + extra files edited outside of Dyad

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,7 +9,7 @@ const Footer = () => {
       <div className="container mx-auto px-4 py-12">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8 text-center md:text-left">
           {/* Column 1: About */}
-          <div className="space-y-4 flex flex-col items-center md:items-start">
+          <div className="space-y-2 flex flex-col items-center md:items-start">
             <Link to="/" className="flex items-center gap-3 mb-2">
               <img src={logoSrc} alt="Pata Livre" className="h-[40px] w-auto" />
             </Link>
