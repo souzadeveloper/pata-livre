4ef86773773601c5f4affe494cedbc330dee900f
Marcelo Souza <souzadeveloper@gmail.com>
2025-09-10 21:42:51 -0300

    [dyad] Increasing WhatsApp icon size on buttons - wrote 3 file(s)

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -22,7 +22,7 @@ const Contact = () => {
               >
                 <svg
                   aria-hidden="true"
-                  className="h-5 w-5"
+                  className="h-6 w-6 mr-2"
                   viewBox="0 0 448 512"
                   xmlns="http://www.w3.org/2000/svg"
                   fill="currentColor"
