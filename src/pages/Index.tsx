28f0f4054191eda383d1475b835e634551f353ad
Marcelo Souza <souzadeveloper@gmail.com>
2025-09-10 22:21:16 -0300

    [dyad] Adjusting WhatsApp icon size - wrote 3 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -145,7 +145,7 @@ const Index = () => {
             >
               <svg
                 aria-hidden="true"
-                className="!h-9 !w-9"
+                className="!h-6 !w-6"
                 viewBox="0 0 448 512"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="currentColor"
